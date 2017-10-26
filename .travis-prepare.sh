a29cc7ad5e915ee3de0d557fd02aae2e4cb25d49
Thys ten Veldhuis <t.tenveldhuis@gmail.com>
2017-10-26 10:59:44 +0200

    Added osx requirement to .travis.yml - Trying to get Travis CI to work using macOS server.

---
+++ .travis-prepare.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+mkdir -p $HOME/.bin
+cd $HOME/.bin
+
+if [ '!' -d apache-maven-3.5.0. ]
+then
+  if [ '!' -f apache-maven-3.5.0-bin.zip ]
+  then 
+    wget https://archive.apache.org/dist/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.zip || exit 1
+  fi
+  echo "Installing maven 3.5.0"
+  unzip -qq apache-maven-3.5.0-bin.zip || exit 1
+  rm -f apache-maven-3.5.0-bin.zip
+fi
+
+exit 0
