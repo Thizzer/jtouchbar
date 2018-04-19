643b6d8512e034dfc1155696fa52b99997f95c79
Thys ten Veldhuis <t.tenveldhuis@gmail.com>
2018-04-19 16:10:32 +0200

    Separate script for staging due to quirck in TRAVIS_BRANCH variable.

--- .travis-prepare.sh
+++ .travis-prepare.sh
@@ -15,16 +15,4 @@ then
   rm -f apache-maven-3.5.0-bin.zip
 fi
 
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-  echo "Unpacking codesigning keys."
-
-  # prepare GPG key
-  openssl aes-256-cbc -K $encrypted_24d52a5d9423_key -iv $encrypted_24d52a5d9423_iv -in "$TRAVIS_BUILD_DIR/.travis/codesigning.asc.enc" -out "$TRAVIS_BUILD_DIR/.travis/codesigning.asc" -d
-
-  echo "Importing codesigning keys."
-  gpg --fast-import "$TRAVIS_BUILD_DIR/.travis/codesigning.asc"
-else
-  echo "Not matching conditions for unpacking codesigning keys."
-fi
-
 exit 0
---
+++ .travis-staging.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+  echo "Unpacking codesigning keys."
+
+  # prepare GPG key
+  openssl aes-256-cbc -K $encrypted_24d52a5d9423_key -iv $encrypted_24d52a5d9423_iv -in "$TRAVIS_BUILD_DIR/.travis/codesigning.asc.enc" -out "$TRAVIS_BUILD_DIR/.travis/codesigning.asc" -d
+
+  echo "Importing codesigning keys."
+  gpg --fast-import "$TRAVIS_BUILD_DIR/.travis/codesigning.asc"
+fi
+
+exit 0
