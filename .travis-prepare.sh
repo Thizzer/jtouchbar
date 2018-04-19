2e439abe849ff45de22b1a3ef83eb608278ed8c6
Thys ten Veldhuis <t.tenveldhuis@gmail.com>
2018-04-19 15:53:24 +0200

    Additional fix for path to codesigning keys for Travis-CI.

--- .travis-prepare.sh
+++ .travis-prepare.sh
@@ -19,7 +19,7 @@ if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; the
   echo "Unpacking codesigning keys."
 
   # prepare GPG key
-  openssl aes-256-cbc -K $encrypted_24d52a5d9423_key -iv $encrypted_24d52a5d9423_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
+  openssl aes-256-cbc -K $encrypted_24d52a5d9423_key -iv $encrypted_24d52a5d9423_iv -in "$TRAVIS_BUILD_DIR/.travis/codesigning.asc.enc" -out "$TRAVIS_BUILD_DIR/.travis/codesigning.asc" -d
 
   echo "Importing codesigning keys."
   gpg --fast-import "$TRAVIS_BUILD_DIR/.travis/codesigning.asc"
