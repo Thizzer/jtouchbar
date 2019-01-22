cebff47bdb6e94ce49014910a7819e0dc61c73a1
Thys ten Veldhuis <t.tenveldhuis@gmail.com>
2019-01-22 22:30:32 +0100

    Updated file headers.

--- src/main/java/com/thizzer/jtouchbar/common/Image.java
+++ src/main/java/com/thizzer/jtouchbar/common/Image.java
@@ -1,3 +1,13 @@
+/**
+ * JTouchBar
+ *
+ * Copyright (c) 2018 - 2019 thizzer.com
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ *
+ * @author  	M. ten Veldhuis
+ */
 package com.thizzer.jtouchbar.common;
 
 import java.io.ByteArrayOutputStream;
--- src/main/objective-c++/Bridged/JavaPopoverTouchBarItem.mm
+++ src/main/objective-c++/Bridged/JavaPopoverTouchBarItem.mm
@@ -1,7 +1,7 @@
 /**
  * JTouchBar
  *
- * Copyright (c) 2018 thizzer.com
+ * Copyright (c) 2018 - 2019 thizzer.com
  *
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE file for details.
